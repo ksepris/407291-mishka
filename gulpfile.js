5d09351a8256fe9076bba4076d6c6ff8970672a2
ksepris <ksepris@yandex.ru>
2018-02-17 21:01:06 +0300

    финальная версия

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var server = require("browser-sync").create();
 var run = require("run-sequence");
-// var del = require("del");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -23,10 +23,10 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
@@ -36,7 +36,7 @@ gulp.task("sprite", function () {
     inlineSvg: true
   }))
   .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("html", function () {
@@ -44,7 +44,7 @@ gulp.task("html", function () {
   .pipe(posthtml([
     include()
   ]))
-    .pipe(gulp.dest("source"));
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("images", function () {
@@ -65,7 +65,7 @@ gulp.task("webp", function () {
 
 gulp.task("serve", function() {
   server.init({
-    server: "source/"
+    server: "build/"
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
@@ -74,8 +74,8 @@ gulp.task("serve", function() {
 
 gulp.task("build", function(done){
   run(
-   // "clean",
-   // "copy",
+   "clean",
+   "copy",
    "style",
    "sprite",
    "html",
@@ -83,17 +83,17 @@ gulp.task("build", function(done){
  );
 });
 
-// gulp.task("copy", function () {
-//   return gulp.src([
-//     "source/fonts/**/*.{woff,woff2}",
-//     "source/img/**",
-//     "source/js/**"
-//   ], {
-//     base: "source"
-//   })
-//   .pipe(gulp.dest("build"));
-// });
-//
-// gulp.task("clean", function () {
-//   return del("build");
-// });
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
